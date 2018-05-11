094c5aac11412333d67b992bf42d4185ff02b913
treehouse-robertpm <robert.manolsi@teamtreehouse.com>
2018-05-11 01:46:06 -0700

    adjusted nav styles

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,9 +8,9 @@ const Home = (props) => {
         <div className="home">
             <h2>{props.heading}</h2>
 
-            <p><strong>Well, what are you witing for?</strong></p>
-            <p><strong>In the navigation menu above, you can search for something of your liking.</strong></p>
-            <p><strong>Or check out some of the "Cool Stuff" I queued up for you. </strong><span role="img" aria-label="Search Icon">&#x1F60E;</span></p>
+            <p><strong>Well, what are you waiting for?</strong></p>
+            <p><strong>In the navigation menu above, you can <span id="searchSpan">Search</span> for something of your liking.</strong></p>
+            <p><strong>Or check out some of the <span id="coolStuffSpan">Cool Stuff</span> I queued up for you. </strong><span role="img" aria-label="Search Icon">&#x1F60E;</span></p>
             <br />
 
             <img src={comingSoon} alt="Coming Soon" />           
